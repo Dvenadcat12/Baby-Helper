757416915c03f720e3d012bb25a3f827a1ba1722
Тимур Реджепов <redzhepov.2004@gmail.com>
2025-11-16 19:05:53 +0300

    add Room, Lifecycle, Coroutines and Gson dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,25 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    // Lifecycle / ViewModel / LiveData
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    // RecyclerView
+    implementation(libs.androidx.recyclerview)
+    // Gson
+    implementation(libs.gson)
+    // Material components
+    implementation(libs.material)
+
 }
